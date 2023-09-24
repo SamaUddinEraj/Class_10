0c3a891b5a3d5bd33114e4f8f332a6d460e3230f
Sama Uddin Eraj <101246963+SamaUddinEraj@users.noreply.github.com>
2023-09-24 23:01:58 +0600

    Homework

---
+++ class_10/class_10.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./Form.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+ 
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <div class="container-fluid">
+  
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Sama Uddin Eraj</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./class_10.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./Form.php">Form</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="./list.php">List</a>
+      </li>
+    </ul>
+    <a class="navbar-brand" href="./class_10.php">
+      <img src="./CV pic 2.jpeg" alt="Logo" style="width:40px;" class="rounded-pill">
+    </a>
+  </div>
+</nav>
+
+
+   <!-- <div class="container-fluid h-100">
+   <h1>Welcome to my website !!!</h1>
+   <p>I want to add a list to control my dashboard</p>
+   
+   </div> -->
+</body>
+</html>
